a7a3389591e36c331a0724cebe01f3c0b8f73ea0
Javier Rodriguez <javicerodriguez@gmail.com>
2023-08-29 00:03:31 -0300

    fix: typo app name folder and files

--- Practice/Practice/asgi.py
+++ Practice/Practice/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Pracitce project.
+ASGI config for Practice project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pracitce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Practice.settings')
 
 application = get_asgi_application()
--- Practice/Practice/urls.py
+++ Practice/Practice/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Pracitce project.
+URL configuration for Practice project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
